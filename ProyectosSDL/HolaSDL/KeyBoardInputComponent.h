afb4627114ed3efd15337390def600fb18525de8
Marcos García García <marcos13@ucm.es>
2018-02-26 18:08:57 +0100

    Added Messages.h & Interact Button on KBComponent

--- ProyectosSDL/HolaSDL/KeyBoardInputComponent.h
+++ ProyectosSDL/HolaSDL/KeyBoardInputComponent.h
@@ -5,7 +5,7 @@ class KeyBoardInputComponent :
 {
 public:
 	KeyBoardInputComponent();
-	KeyBoardInputComponent(SDL_Scancode left, SDL_Scancode right, SDL_Scancode up, SDL_Scancode down, SDL_Scancode attack, SDL_Scancode inventory, SDL_Scancode pause, SDL_Scancode enter);
+	KeyBoardInputComponent(SDL_Scancode left, SDL_Scancode right, SDL_Scancode up, SDL_Scancode down, SDL_Scancode interact, SDL_Scancode attack, SDL_Scancode inventory, SDL_Scancode pause, SDL_Scancode enter);
 	virtual ~KeyBoardInputComponent();
 	virtual void handleInput(GameObject* o, Uint32 time, const SDL_Event& event);
 
@@ -14,6 +14,7 @@ private:
 	SDL_Scancode right_;
 	SDL_Scancode up_;
 	SDL_Scancode down_;
+	SDL_Scancode interact_;
 	SDL_Scancode attack_;
 	SDL_Scancode inventory_;
 	SDL_Scancode pause_;

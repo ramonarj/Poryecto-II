7b8e287b176bbeb78c127fc4a16cfb0bd8674efa
Ramón <ramonarj@ucm.es>
2018-03-12 16:57:48 +0100

    Finished rudimentary enemy logic

--- ProyectosSDL/HolaSDL/Enemy.cpp
+++ ProyectosSDL/HolaSDL/Enemy.cpp
@@ -14,52 +14,66 @@ Enemy::Enemy(Entity* player, int life, int damage):player(player), Character(lif
 
 void Enemy::move(Entity* o)
 {
-	//Posición del jugador y del enemigo
-	Vector2D pos{ o->getPosition().getX(), o->getPosition().getY() };
-	Vector2D playerPos{ player->getPosition().getX(), player->getPosition().getY() };
+	Entity* player = Game::Instance()->getEntityWithComponent<Player>();
 
-	//Lo persigue
-	Vector2D chaseVector{ playerPos.getX() - pos.getX(), playerPos.getY() - pos.getY() };
-	float alpha = atan(chaseVector.getY() / chaseVector.getX());
-	int velMag = o->getVelocity().magnitude();
-
-	//Ponemos la nueva velocidad
-	Vector2D vel{ cos(alpha) * velMag, sin(alpha)* velMag};
-	cout << vel.getX() << " " << vel.getY() << endl;
-	//o->setVelocity(vel);
-
-	//Movimiento en X
-	if (pos.getX() < playerPos.getX())
-		pos.setX(pos.getX() + 1);
-	else if (pos.getX() > playerPos.getX())
-		pos.setX(pos.getX() - 1);
-
-	//Movimiento en Y
-	if (pos.getY() < playerPos.getY())
-		pos.setY(pos.getY() + 1);
-	else if (pos.getY()> playerPos.getY())
-		pos.setY(pos.getY() - 1);
-
-	//Actualizamos a posicion(PONER ESTO EN CHARACTER)
-	pos.setX(pos.getX() + o->getVelocity().getX());
-	pos.setY(pos.getY() + o->getVelocity().getY());
-
-	o->setPosition(pos);
-
-
-	//PROVISIONAL: HACER COLISIONES EN PLAN BIEN
-	//Lo pilla
-	if (pos.getX() == playerPos.getX() && pos.getY() == playerPos.getY())
+	//Si el jugador está vivo
+	if(player->getComponent<Player>()->isAlive())
 	{
-		//De momento, ambos se hacen daño
-		player->getComponent<Player>()->takeDamage(damage);
-		this->takeDamage(player->getComponent<Player>()->getDamage());
+		//Posición del jugador y del enemigo
+		Vector2D pos{ o->getPosition().getX(), o->getPosition().getY() };
+		Vector2D playerPos{ player->getPosition().getX(), player->getPosition().getY() };
 
-		//Respawn
-		pos.setX(pos.getX() + 20);
-		pos.setY(pos.getY() + 20);
+		//Lo persigue
+		Vector2D chaseVector{ playerPos.getX() - pos.getX(), playerPos.getY() - pos.getY() };
+		float alpha = abs(atan(chaseVector.getY() / chaseVector.getX()));
+		int velMag = o->getVelocity().magnitude();
+
+		////Ponemos la nueva velocidad
+		//Vector2D vel{ cos(alpha) * velMag * chaseVector.getX() / abs(chaseVector.getX()), sin(alpha)* velMag * chaseVector.getY() / abs(chaseVector.getY()) };
+		//cout << vel << endl;
+		//o->setVelocity(vel);
+
+		////Actualizamos a posicion(PONER ESTO EN CHARACTER)
+		//pos.setX(pos.getX() + o->getVelocity().getX());
+		//pos.setY(pos.getY() + o->getVelocity().getY());
+
 		o->setPosition(pos);
+		Vector2D vel;
+		//Movimiento en X
+		if (pos.getX() < playerPos.getX())
+			vel.setX(velMag);
+		else if (pos.getX() > playerPos.getX())
+			vel.setX(-velMag);
+
+		//Movimiento en Y
+		if (pos.getY() < playerPos.getY())
+			vel.setY(velMag);
+		else if (pos.getY()> playerPos.getY())
+			vel.setY(-velMag);
+
+		o->setVelocity(vel);
+		//Actualizamos a posicion(PONER ESTO EN CHARACTER)
+		pos.setX(pos.getX() + o->getVelocity().getX());
+		pos.setY(pos.getY() + o->getVelocity().getY());
+
+		o->setPosition(pos);
+
+
+		//PROVISIONAL: HACER COLISIONES EN PLAN BIEN
+		//Lo pilla
+		if (pos.getX() == playerPos.getX() && pos.getY() == playerPos.getY())
+		{
+			//De momento, ambos se hacen daño
+			player->getComponent<Player>()->takeDamage(damage);
+			this->takeDamage(player->getComponent<Player>()->getDamage());
+
+			//Respawn
+			pos.setX(pos.getX() + 30);
+			pos.setY(pos.getY() + 30);
+			o->setPosition(pos);
+		}
 	}
+	
 }
 
 void Enemy::handleInput(Entity* o, Uint32 time, const SDL_Event& event) {}
@@ -68,12 +82,10 @@ void Enemy::render(Entity* o, Uint32 time) {}
 void Enemy::update(Entity * o, Uint32 time)
 {
 	//Tanto él como el jugador están vivos
-	if (isAlive() && player != nullptr && player->getComponent<Player>()->isAlive())
+	if (isAlive())
 		move(o);
-
-	//Si no, se queda quieto
-	/*else
-		cout << "(" << o->getPosition().getX() << "," << o->getPosition().getY() << ")" << endl;*/
+	else
+		o->setVelocity(Vector2D(0, 0));
 }
 
 Enemy::~Enemy()

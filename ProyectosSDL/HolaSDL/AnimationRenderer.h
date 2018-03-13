6df62d03e3fe3cbd06b48c27a0ee15badd90dc9f
Test <placeHolder@placeHolder.com>
2018-03-13 17:47:58 +0100

    Animaciones terminadas
    y modificado el tipo de spritesheet para personajes

--- ProyectosSDL/HolaSDL/AnimationRenderer.h
+++ ProyectosSDL/HolaSDL/AnimationRenderer.h
@@ -7,19 +7,19 @@ class AnimationRenderer :
 	public Component
 {
 public:
-	AnimationRenderer(Texture* image, Texture* imageAttack, Uint32 movementFrames, Uint32 attackMovementFrames, bool character);
+	AnimationRenderer(Texture* image, Uint32 movementFrames, Uint32 attackMovementFrames, Uint32 cooldown, bool character, bool direccionable);
 	virtual ~AnimationRenderer();
 	virtual void render(Entity* o, Uint32 time);
 	virtual void update(Entity* o, Uint32 time) {};
 	virtual void handleInput(Entity* o, Uint32 time, const SDL_Event& event) {};
 
 private:
-	Texture *image_, *imageAttack_;
-	Uint32 movementFrames_, movements_ = 4, frame_ = 0, idleFrame_ = 0, actualTime_ = 0, cooldown_ = 80, cooldownIddle_ = 400;
-	Uint32 attackMovementFrames_, attackFrames_ = 0;
+	Texture *image_;
+	Uint32 movementFrames_, movements_ = 4, frame_ = 0, idleFrame_ = 0, actualTime_ = 0, cooldown_, cooldownIddle_ = cooldown_*5;
+	Uint32 attackMovementFrames_, attackFrames_ = 14;
 	Vector2D lastDir;
 	int dir(Entity* o);
 	int dirIddle(Entity* o);
-	bool character;
+	bool character, direccionable;
 };
 

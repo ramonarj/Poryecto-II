fe8c9883ecffbaccfbc76ffadd1f8ceca0065307
Kelvin-CD <kelvinco@ucm.es>
2018-04-03 16:44:34 +0200

    Added Fonts to Resource Manager

--- ProyectosSDL/HolaSDL/ResourceManager.cpp
+++ ProyectosSDL/HolaSDL/ResourceManager.cpp
@@ -1,42 +1,44 @@
 #include "ResourceManager.h"
 #include "Game.h"
 
-
-ResourceManager::ResourceManager(Game* game) : game_(game)
-{
+ResourceManager::ResourceManager(Game* game) : game_(game) {
 }
 
-
-ResourceManager::~ResourceManager()
-{
+ResourceManager::~ResourceManager() {
 }
 
-void ResourceManager::addTexture(string id, string filepath)
-{
+// Textures ------------------------------------------------------------
+void ResourceManager::addTexture(string id, string filepath) {
 	textures_.insert(pair<string, Texture*>(id, new Texture(game_->getRenderer(), filepath)));
 }
 
-Texture * ResourceManager::getTexture(string id)
-{
+Texture * ResourceManager::getTexture(string id) {
 	return textures_[id];
 }
 
-void ResourceManager::addMusic(string id, string filepath)
-{
+// Music ------------------------------------------------------------
+void ResourceManager::addMusic(string id, string filepath) {
 	music_.insert(pair<string, Music*>(id, new Music(filepath)));
 }
 
-Music * ResourceManager::getMusic(string id)
-{
+Music * ResourceManager::getMusic(string id) {
 	return music_[id];
 }
 
-void ResourceManager::addSound(string id, string filepath)
-{
+// Sounds ------------------------------------------------------------
+void ResourceManager::addSound(string id, string filepath) {
 	sounds_.insert(pair<string,SoundEffect*>(id, new SoundEffect(filepath)));
 }
 
-SoundEffect * ResourceManager::getSound(string id)
-{
+SoundEffect * ResourceManager::getSound(string id) {
 	return sounds_[id];
+}
+
+// Fonts ------------------------------------------------------------
+void ResourceManager::addFont(string id, string filepath, int fontSize) {
+	fonts_.insert(pair<string, Font*>(id, new Font(filepath, fontSize)));
+}
+
+Font * ResourceManager::getFont(string id) {
+	return fonts_[id];
 }
